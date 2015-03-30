41b7e5000b0433659adce27aaa3e37bfab973ff4
Ajay Kulkarni <ajaykulkarni178@gmail.com>
2015-03-30 19:03:34 +0530

    Second commit, additional features were added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -95,7 +95,7 @@
 	</div>
 
 <div class="container marketing">
-    <h2 class="itemsTitle"><center>Breakfast</center></h2>
+    <h2 class="itemsTitle"><center>Available Foods</center></h2>
 	<div id="myCarousel1" class="carousel slide" data-ride="carousel">
       <!-- Indicators -->
       <div class="carousel-inner">
@@ -113,7 +113,7 @@
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <center><img src="/images/drinks.jpeg" alt="Generic placeholder image"></center>
-          <h4><center>Drink</center></h4>
+          <h4><center>Drinks</center></h4>
           <center><p><a class="btn btn-default" href="#" role="button">Add to cart &raquo;</a></p></center>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
@@ -133,7 +133,7 @@
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <center><img src="/images/drinks.jpeg" alt="Generic placeholder image"></center>
-          <h4><center>Drink</center>></h4>
+          <h4><center>Drinks</center>></h4>
           <center><p><a class="btn btn-default" href="#" role="button">Add to cart &raquo;</a></p></center>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
@@ -165,9 +165,49 @@
 	</div>
 
 
+<div class="introSection">
+		<div class="container">
+		<div class="row">
+		<div class="col-lg-5">
+		<h3>Welcome to restaurant</h3>
+		<p>
+		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+		text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+		It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. <br><br>
+		It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+		and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+		</p>
+		</div>
+		
+		<div class="col-lg-4">
+		<h3>Welcome to restaurant</h3>
+		<p>
+		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+		text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+		It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. <br><br>
+		
+		</p>
+		
+		</div>
+		
+		<div class="col-lg-3">
+		<h3>Welcome to restaurant</h3>
+		<p>
+		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+		text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+		
+		</p>
+		
+		</div>
+		
+		</div>
+		</div>
+
+
 	</div>
 </div>
-<div class="footer">
-	<p>This is a footer</p>
-</div>
+<footer>
+	<p class="pull-right"><a href="#">Back to top</a></p>
+    <center><p class="copyright">&copy; Copyright 2015 Ajay Kulkarni</center>
+</footer>
 @endsection
